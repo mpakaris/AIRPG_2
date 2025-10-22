bdbd4e63fc71522aaf57b57488b33625dfb2b425
Mpakaris Nikos <nikos.mpakaris@gmail.com>
2025-10-22 16:49:53 +0000

    After major Change: Examine/Check/Move etc. on Objects seems to work

--- src/lib/game/cartridge.ts
+++ src/lib/game/cartridge.ts
@@ -487,7 +487,7 @@ const chapters: Record<ChapterId, Chapter> = {
         id: 'ch1-the-cafe' as ChapterId,
         title: 'A Blast from the Past',
         goal: "Unlock the contents of the notebook.",
-        introductionVideo: 'https://res.cloudinary.com/dg912bwcc/image/upload/v1759670681/CH_I_Intro_ccy0og.mp4',
+        introductionVideo: 'https://res.cloudinary.com/dg912bwcc/video/upload/f_mp4/v1759670681/CH_I_Intro_ccy0og.mp4',
         completionVideo: 'https://res.cloudinary.com/dg912bwcc/video/upload/v1759678377/CH_I_completion_jqtyme.mp4',
         postChapterMessage: "Looks like we've got everything from this place. I'm thinking our next stop should be the jazz club mentioned in the article.",
         nextChapter: {
